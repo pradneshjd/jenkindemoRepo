2aecc9426ff5f01d3b928b1d9048d3e2aa50f7f0
Pradnesh Jadhav <pradnesh.jadhav@morningstar.com>
2022-11-25 10:56:42 +0530

    first commit

---
+++ jenkindemo/Abc.java
@@ -0,0 +1,12 @@
+package jenkindemo;
+
+public class Abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i = 0; i < 5; i++) {
+	System.out.println("Hello jenkin"+i);
+}
+	}
+
+}
